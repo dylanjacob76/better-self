d9f01eb3b6190a4221889c44abe3744c190e9b06
Miyamoto Musashi <business.dmjacob@gmail.com>
2023-04-03 08:31:35 -0400

    edit for railway in the connection.js

--- config/connection.js
+++ config/connection.js
@@ -3,19 +3,15 @@ require("dotenv").config();
 
 let sequelize;
 
-if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
-} else {
-  sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASS,
-    {
-      host: "localhost",
-      dialect: "mysql",
-      port: 3306
-    }
-  );
-}
+sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASS,
+  {
+    host: process.env.MYSQLHOST,
+    dialect: 'mysql',
+    port: process.env.MYSQLPORT
+  }
+);
 
 module.exports = sequelize;
